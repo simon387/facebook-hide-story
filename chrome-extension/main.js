4af29092d5ea7aa5f578e581c4d478bda6f91504
Simone Celia <simone.celia@simonecelia.it>
2023-03-09 23:54:35 +0100

    refa

--- chrome-extension/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('[aria-label="Crea storia"]').parentElement.parentElement.parentElement.remove();
-console.log('lele');
-// document.addEventListener('DOMContentLoaded', function() {
-// 	console.log('loaded');
-// 	document.querySelector('[aria-label="Crea storia"]').parentElement.parentElement.parentElement.remove();
-// });
