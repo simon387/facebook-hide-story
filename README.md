35f92513f35f21e1c506bde63b2b3d01379f18ce
Simone Celia <simone.celia@simonecelia.it>
2023-03-09 23:43:19 +0100

    inti

---
+++ README.md
@@ -0,0 +1,8 @@
+# Facebook Hide Story
+
+Hide facebook story - nasconde storie facebook
+
+## setup
+
++ load chrome-extension as unpacked
++ enjoy
---
+++ chrome-extension/main.js
@@ -0,0 +1,6 @@
+document.querySelector('[aria-label="Crea storia"]').parentElement.parentElement.parentElement.remove();
+console.log('lele');
+// document.addEventListener('DOMContentLoaded', function() {
+// 	console.log('loaded');
+// 	document.querySelector('[aria-label="Crea storia"]').parentElement.parentElement.parentElement.remove();
+// });
